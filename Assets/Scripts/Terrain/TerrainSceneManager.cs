061bb6748a317026d14d627fb71b3cca1ef0b938
Felix Timm <felix.timm2404@gmail.com>
2024-12-18 22:06:10 +0100

    reworking idea

--- Assets/Scripts/Terrain/TerrainSceneManager.cs
+++ Assets/Scripts/Terrain/TerrainSceneManager.cs
@@ -1,10 +1,35 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
+
 public class TerrainSceneManager : MonoBehaviour
 {
-    Scene scenes;
-    private void Start()
+    [Header("Player")]
+    [SerializeField] private GameObject player;
+
+    [Header("Terrain Data")]
+    [Header("Read only")]
+    [SerializeField] private int chunk;
+
+    private Vector3 playerPosition;
+    
+   
+
+    private void Update()
     {
+        playerPosition = player.transform.position;
+        GetChunk();
         
     }
+
+    private void GetChunk()
+    {
+        //get relative player Position
+        int p_x = (int) Mathf.Ceil(playerPosition.x / 100F);
+        int p_z = (int) Mathf.Ceil(playerPosition.z / 100F);
+
+        chunk = (20 * p_z) + p_x;
+    }
+
+
 }
