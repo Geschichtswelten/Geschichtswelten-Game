e63bb877ec1ffc29e47e9c515fa5658f0899e282
Felix Timm <felix.timm2404@gmail.com>
2025-01-25 15:47:10 +0100

    did some stuff

--- Assets/Scripts/MainGameLoop.cs
+++ Assets/Scripts/MainGameLoop.cs
@@ -1,17 +1,31 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainGameLoop : MonoBehaviour
 {
     public GameObject doorLeft, doorRight;
     public BoxCollider endCollider;
+    public GameObject endingCanvas;
+
+    public bool killedArminius = true;
+    public VideoClip[] videoClips;
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        DontDestroyOnLoad(this);
+    }
 
     public void EndingSequence()
     {
         doorLeft.transform.Rotate(0, -89, 0);
         doorRight.transform.Rotate(0, 89, 0);
         endCollider.gameObject.SetActive(true);
+        Instantiate(endingCanvas, Vector3.zero, Quaternion.identity);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,12 +41,28 @@ public class MainGameLoop : MonoBehaviour
 
     private IEnumerator EndGame()
     {
-        AsyncOperation loadScene = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
-        loadScene.allowSceneActivation = false;
-        JsonHandler.DeleteGameProfile();
-        //start Video
-        yield return new WaitUntil(() => true);     //video.isDone
+        AsyncOperation loadScene = SceneManager.LoadSceneAsync(394, LoadSceneMode.Single);
         loadScene.allowSceneActivation = true;
+        JsonHandler.DeleteGameProfile();
+        if (killedArminius)
+        {
+            videoPlayer.clip = videoClips[0];
+        }
+        else
+        {
+            videoPlayer.clip = videoClips[1];
+        }
+        videoPlayer.SetDirectAudioVolume(0, ButtonHandler.settings.dialogueVolume * ButtonHandler.settings.masterVolume);
+        yield return new WaitUntil(()=>loadScene.isDone);
+        //start Video
+        yield return new WaitUntil(() => !videoPlayer.isPlaying);     //video.isDone
+        var loadingScreen = GameObject.FindGameObjectWithTag("Finish");
+        loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        loadScene = SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        loadScene.allowSceneActivation = true;
+        yield return new WaitUntil(()=>loadScene.isDone);
+        Destroy(this.gameObject);
     }
 
 }
